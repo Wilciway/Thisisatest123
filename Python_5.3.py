9095c0ebb1722fc29d3a8f2ddaecc92a4986a599
wilciway <wilco.matthijssen@student.hu.nl>
2018-09-12 14:55:42 +0200

    Python_5.4

--- Python_5.3.py
+++ Python_5.3.py
@@ -2,3 +2,5 @@ leeftijd=input("Hoe oud ben je?: ")
 paspoort=input("Bezit je een Nederlands paspoort?: ")
 if int(leeftijd)>=18 and paspoort == "ja":
    print("Gefeliciteerd, je mag stemmen!")
+else:
+    print("Je mag niet stemmen")
---
+++ Python_5.4.py
@@ -0,0 +1,4 @@
+dagen=["maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag","zondag"]
+for dag in dagen:
+    print(dag[:2])
+
