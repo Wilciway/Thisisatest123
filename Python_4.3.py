e655bc6b6dd7092b3a0178f96673c8a1ef86d8de
wilciway <wilco.matthijssen@student.hu.nl>
2018-09-12 13:50:36 +0200

    Python_4.3

---
+++ Python_4.3.py
@@ -0,0 +1,4 @@
+uurloon = int(input("Wat verdien je per uur: "))
+werkuren = int(input("Hoeveel uur heb je gewerkt: "))
+salaris = uurloon * werkuren
+print(werkuren, " uur werken levert ", float(salaris), " Euro op")
