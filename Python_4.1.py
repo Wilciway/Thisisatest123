ebaff6c88eb2c03cb2257fc0d3cda86d5390913b
wilciway <wilco.matthijssen@student.hu.nl>
2018-09-12 13:55:01 +0200

    Python_4.1

---
+++ Python_4.1.py
@@ -0,0 +1,9 @@
+euro = 30
+cijferICOR = 10
+cijferPROG = 10
+cijferCSN = 10
+
+geld = (cijferCSN+cijferICOR+cijferICOR)*euro
+gemiddeld = (cijferCSN+cijferICOR+cijferICOR)/3
+
+print("Mijn cijfers zijn gemiddeld een",gemiddeld,"wat",geld,"oplevert")
