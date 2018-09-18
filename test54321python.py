68daf4210831a284aa26fa90e83a55fb676de802
wilciway <wilco.matthijssen@student.hu.nl>
2018-09-18 09:59:05 +0200

    Python_6.5

--- test54321python.py
+++ test54321python.py
@@ -1,10 +1,8 @@
- score=input("Geef je score: ")
-if float(score)>15:
-  print("Gefeliciteerd")
-    uitkomst="geslaagd!"
 
-else:
-    print("Jammer!")
-   uitkomst="gezakt!"
+def new_password(oldpassword,newpassword):
+    if (oldpassword == newpassword)==False and len(newpassword) >=6 and any(i.isdigit() for i in newpassword):
+        return True
+    else:
+        return False
+new_password("test1234","test1234567")
 
-print("Met een score van ",score," ben je ",uitkomst)
